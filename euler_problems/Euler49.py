c0bac054bdb2eb1d3acf2af36f35765d2627e309
ethanbond64 <ethanbond39@gmail.com>
2020-05-05 01:11:01 -0500

    completed 50

--- euler_problems/Euler49.py
+++ euler_problems/Euler49.py
@@ -1,4 +1,6 @@
+import copy
 import math
+
 primes = [0]
 num = 2
 while primes[-1] < 10000:
@@ -16,4 +18,34 @@ for x in primes:
     if x >= 1000:
         nuPrimes.append(x)
 
-def permList()
+dic = {}
+for idx, k in enumerate(nuPrimes):
+    x = sorted(str(k))
+    for j in nuPrimes[idx+1:]:
+        if sorted(str(j)) == x:
+            t = str(x)[1:-1].replace(', ','').replace("'",'')
+            if t in dic:
+                dic[t].append(j)
+            else:
+                dic[t] = [k,j]
+
+d = copy.deepcopy(dic)
+
+for key in d:
+    if len(dic[key]) < 3:
+        del dic[key]
+    else:
+        dic[key] = list(set(dic[key]))
+        dic[key] = sorted(dic[key])
+        ll = dic[key]
+        for el in ll:
+            diffs = []
+            for el2 in ll:
+                if el == el2:
+                    pass
+                if abs(el-el2) in diffs:
+                    print(ll,abs(el-el2))
+                diffs.append(abs(el-el2))
+
+# print(dic)
+
